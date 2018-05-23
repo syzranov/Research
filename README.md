6ee507a84657f83e08b1a270240e9a0a4bdee521
Andrey Syzranov <syzranov@gmail.com>
2018-05-23 09:48:18 +0300

    first compile ExampleProgram

---
+++ README.md
@@ -0,0 +1,1 @@
+Here java reasearch mini projects
---
+++ lesson_1/ExampleProgram.java
@@ -0,0 +1,9 @@
+
+//A Very Simple Example
+public class ExampleProgram {
+  public static void main(String[] args){
+    System.out.println("I'm a Simple Program");
+
+    System.out.println("I'm a Simple Program");
+  }
+}
